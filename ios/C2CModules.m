b430636818821dc0bec65919913971e889507de7
Le Hau <hau.le@c2c-techhub.io>
2020-10-15 15:27:41 +0700

    fix: convert phasset (video) to avasset

--- ios/C2CModules.m
+++ ios/C2CModules.m
@@ -2,6 +2,11 @@
 
 @interface RCT_EXTERN_MODULE(C2CModules, NSObject)
 
++ (BOOL)requiresMainQueueSetup
+{
+    return YES;
+}
+
 RCT_EXTERN_METHOD(convertPHAsset:(NSDictionary *)params
                 withResolver:(RCTPromiseResolveBlock)resolve
                 withRejecter:(RCTPromiseRejectBlock)reject)
