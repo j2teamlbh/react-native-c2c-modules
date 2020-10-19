c3dda2b64a2a20e134a901ab9043c2fb7ca87524
Le Hau <j2team.lbh@gmail.com>
2020-10-20 00:26:38 +0700

    fix: update fullscreen android

--- android/src/main/java/com/reactnativec2cmodules/C2CModulesModule.kt
+++ android/src/main/java/com/reactnativec2cmodules/C2CModulesModule.kt
@@ -16,17 +16,16 @@
     fun onFullScreen() {
       UiThreadUtil.runOnUiThread {
         currentActivity!!.window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_STABLE or
-          View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION or
-          View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or
-          View.SYSTEM_UI_FLAG_HIDE_NAVIGATION or
-          View.SYSTEM_UI_FLAG_FULLSCREEN or
-          View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+              View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY or
+              View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
       }
     }
 
     @ReactMethod
     fun offFullScreen() {
-      UiThreadUtil.runOnUiThread { currentActivity!!.window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_STABLE }
+      UiThreadUtil.runOnUiThread { currentActivity!!.window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_STABLE or
+              View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION or
+              View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN }
     }
 
   }
