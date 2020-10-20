67146f1bcefb57bfcd9b97fb5f7816ec0eb8cb8c
Le Hau <hau.le@c2c-techhub.io>
2020-10-20 10:23:29 +0700

    fix: android bug

--- android/src/main/java/com/reactnativec2cmodules/C2CModulesModule.kt
+++ android/src/main/java/com/reactnativec2cmodules/C2CModulesModule.kt
@@ -1,34 +1,31 @@
-  package com.reactnativec2cmodules
+package com.reactnativec2cmodules
 
-  import android.view.View
-  import com.facebook.react.bridge.ReactApplicationContext
-  import com.facebook.react.bridge.ReactContextBaseJavaModule
-  import com.facebook.react.bridge.ReactMethod
-  import com.facebook.react.bridge.UiThreadUtil
+import android.view.View
+import com.facebook.react.bridge.ReactApplicationContext
+import com.facebook.react.bridge.ReactContextBaseJavaModule
+import com.facebook.react.bridge.ReactMethod
+import com.facebook.react.bridge.UiThreadUtil
 
-  class C2CModulesModule(reactContext: ReactApplicationContext) : ReactContextBaseJavaModule(reactContext) {
+class C2CModulesModule(reactContext: ReactApplicationContext) : ReactContextBaseJavaModule(reactContext) {
 
-      override fun getName(): String {
-          return "C2CModules"
-      }
-
-    @ReactMethod
-    fun onFullScreen() {
-      UiThreadUtil.runOnUiThread {
-        currentActivity!!.window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LOW_PROFILE or
-                View.SYSTEM_UI_FLAG_FULLSCREEN or
-                View.SYSTEM_UI_FLAG_LAYOUT_STABLE or
-                View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY or
-                View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION or
-                View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-      }
-    }
-
-    @ReactMethod
-    fun offFullScreen() {
-      UiThreadUtil.runOnUiThread { currentActivity!!.window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_STABLE or
-              View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION or
-              View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN }
-    }
-
+  override fun getName(): String {
+      return "C2CModules"
   }
+
+  @ReactMethod
+  fun onFullScreen() {
+    UiThreadUtil.runOnUiThread {
+      currentActivity!!.window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_STABLE or
+            View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY or
+            View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+    }
+  }
+
+  @ReactMethod
+  fun offFullScreen() {
+    UiThreadUtil.runOnUiThread { currentActivity!!.window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_STABLE or
+            View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION or
+            View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN }
+  }
+
+}
