83085c01ad596ea068220a5e1d58a419defa0d97
Le Hau <hau.le@c2c-techhub.io>
2020-11-04 13:00:31 +0700

    fix: android notch support

--- android/src/main/java/com/reactnativec2cmodules/C2CModulesModule.kt
+++ android/src/main/java/com/reactnativec2cmodules/C2CModulesModule.kt
@@ -15,17 +15,20 @@ class C2CModulesModule(reactContext: ReactApplicationContext) : ReactContextBase
   @ReactMethod
   fun onFullScreen() {
     UiThreadUtil.runOnUiThread {
-      currentActivity!!.window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_STABLE or
-            View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY or
-            View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+      if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+        currentActivity!!.window.attributes.layoutInDisplayCutoutMode = WindowManager.LayoutParams.LAYOUT_IN_DISPLAY_CUTOUT_MODE_SHORT_EDGES
+      }
     }
   }
 
   @ReactMethod
   fun offFullScreen() {
-    UiThreadUtil.runOnUiThread { currentActivity!!.window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_STABLE or
-            View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION or
-            View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN }
+    UiThreadUtil.runOnUiThread {
+      if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+        currentActivity!!.window.attributes.layoutInDisplayCutoutMode = WindowManager.LayoutParams.LAYOUT_IN_DISPLAY_CUTOUT_MODE_DEFAULT
+      }
+    }
   }
+
 
 }
